d3ca4330672955ed1f10b6b816cd16d914c25e45
themarving <111308358+themarving@users.noreply.github.com>
2026-03-12 15:48:03 +0100

    Delete install directory

--- install/custom_interfaces/local/lib/python3.10/dist-packages/custom_interfaces/srv/__init__.py
+++
@@ -1,2 +0,0 @@
-from custom_interfaces.srv._change_face_state import ChangeFaceState  # noqa: F401
-from custom_interfaces.srv._set_custom_navigation_goal import SetCustomNavigationGoal  # noqa: F401
